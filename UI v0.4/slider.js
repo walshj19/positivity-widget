ed949ab5bf53141b32a4fac311e5077c28ecde80
Patrick O'Boyle <oboylepa@tcd.ie>
2014-11-22 13:53:15 +0000

    UI
    To Do:
    -Centre
    -Font
    -Integrate with JS

---
+++ UI v0.4/slider.js
@@ -0,0 +1,46 @@
+
+
+    (function() {
+      var test = $(".testing");
+
+      // store the slider in a local variable
+      var $window = $(window),
+          flexslider;
+
+      // tiny helper function to add breakpoints
+      function getGridSize() {
+        return 3;
+      }
+
+      $(function() {
+        SyntaxHighlighter.all();
+      });
+
+      $window.load(function() {
+        $('.flexslider').flexslider({
+          animation: "slide",
+          animationSpeed: 1200,
+          animationLoop: false,
+          itemWidth: 306,
+          itemMargin: 5,
+          controlNav: false,
+          move: 1,
+          minItems: getGridSize(), // use function to pull in initial value
+          maxItems: getGridSize(), // use function to pull in initial value
+          start: function(slider){
+            $('body').removeClass('loading');
+            flexslider = slider;
+          }
+        });
+      });
+
+      // check grid size on resize event
+      $window.resize(function() {
+        var gridSize = getGridSize();
+
+        flexslider.vars.minItems = gridSize;
+        flexslider.vars.maxItems = gridSize;
+      });
+    }());
+
+  
